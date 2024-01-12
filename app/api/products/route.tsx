bfb01719caff3f797e50de15322ac4076df28de6
Javier <javier@databalk.nu>
2024-01-12 12:32:30 +0200

    DataBase Intergration

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
-import schema from "./shema";
+import schema from "./schema";
+import { prisma } from "@/prisma/client"
 
-export function GET(request: NextResponse) {
-    return NextResponse.json([
-        { id: 1, name: "Milk", price: 2.5 },
-        { id: 2, name: "Bread", price: 3.5 },
-    ])
+export async function GET(request: NextResponse) {
+    const products = await prisma.product.findMany();
+    return NextResponse.json(products)
 }
+
 
 export async function POST(request: NextResponse) {
     const body = await request.json();
     const validation = schema.safeParse(body)
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, { status: 400 });
-    return NextResponse.json({ id: 10, name: body.name, price: body.price }, { status: 201 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+    const newProduct = await prisma.product.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+return NextResponse.json(newProduct, { status: 201 });
 }
+
 
 export async function PUT(request: NextResponse) {
     const body = await request.json();
--- app/api/products/schema.tsx
+++ app/api/products/schema.tsx
@@ -1,7 +1,6 @@
 import { z } from "zod";
-import schema from "../users/shema";
 
-const shema = z.object({
+const schema = z.object({
   name: z.string().min(3),
   price: z.number().min(1).max(100),
 });
